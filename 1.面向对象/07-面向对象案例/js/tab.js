4cdefa1e9d1e84bdee9ab705704b6c8b619814a8
Vacricticy <987540432@qq.com>
2020-06-08 09:07:37 +0800

    review

--- 1.面向对象/07-面向对象案例/js/tab.js
+++ 1.面向对象/07-面向对象案例/js/tab.js
@@ -1,77 +1,77 @@
 var that;
 class Tab {
     constructor(id) {
-            that = this;
-            this.main = document.querySelector(id);
+        that = this;
+        this.main = document.querySelector(id);
 
-            // 获取添加按钮的元素
-            this.add = this.main.querySelector(".tabadd");
-            // 获取li的父元素
-            this.ul = this.main.querySelector(".fisrstnav ul:first-child");
-            // 获取section的父元素
-            this.fsection = this.main.querySelector(".tabscon");
-            this.init();
-        }
-        // 初始化操作，让相关的元素绑定事件
+        // 获取添加按钮的元素
+        this.add = this.main.querySelector(".tabadd");
+        // 获取li的父元素
+        this.ul = this.main.querySelector(".fisrstnav ul:first-child");
+        // 获取section的父元素
+        this.fsection = this.main.querySelector(".tabscon");
+        this.init();
+    };
+    // 初始化操作，让相关的元素绑定事件
     init() {
-            this.updateNode();
-            // 为+绑定添加事件
-            this.add.onclick = this.addTab;
-            // 为li绑定切换事件
-            for (var i = 0; i < this.lis.length; i++) {
-                this.lis[i].index = i;
-                this.lis[i].onclick = this.toggleTab;
-                this.guanbis[i].index = i;
-                this.guanbis[i].onclick = this.deleteTab;
-                // 绑定双击事件
-                this.spans[i].ondblclick = this.editTab;
-                this.sections[i].ondblclick = this.editTab;
-            }
-            // 为关闭按钮绑定关闭事件，简化为上诉操作
-            // for (var i = 0; i < this.guanbis.length; i++) {
-            //     this.guanbis[i].index = i;
-            //     this.guanbis[i].onclick = this.deleteTab;
-            // }
+        this.updateNode();
+        // 为+绑定添加事件
+        this.add.onclick = this.addTab;
+        // 为li绑定切换事件
+        for (var i = 0; i < this.lis.length; i++) {
+            this.lis[i].index = i;
+            this.lis[i].onclick = this.toggleTab;
+            this.guanbis[i].index = i;
+            this.guanbis[i].onclick = this.deleteTab;
+            // 绑定双击事件
+            this.spans[i].ondblclick = this.editTab;
+            this.sections[i].ondblclick = this.editTab;
         }
-        // 重新获取lis和sections
+        // 为关闭按钮绑定关闭事件，简化为上诉操作
+        // for (var i = 0; i < this.guanbis.length; i++) {
+        //     this.guanbis[i].index = i;
+        //     this.guanbis[i].onclick = this.deleteTab;
+        // }
+    };
+    // 重新获取lis和sections
     updateNode() {
-            this.lis = this.main.querySelectorAll("li");
-            this.sections = this.main.querySelectorAll("section");
-            this.guanbis = this.main.querySelectorAll(".icon-guanbi");
-            this.spans = this.main.querySelectorAll(".fisrstnav li span:first-child");
-        }
-        // 切换功能
+        this.lis = this.main.querySelectorAll("li");
+        this.sections = this.main.querySelectorAll("section");
+        this.guanbis = this.main.querySelectorAll(".icon-guanbi");
+        this.spans = this.main.querySelectorAll(".fisrstnav li span:first-child");
+    };
+    // 切换功能
     toggleTab() {
-            // 该函数的调用者为li,所以函数内部的this指向的都是li元素，clearClass函数是实例对象的函数，所有只能通过that调用
-            that.clearClass();
-            // 该函数的调用者为li,所以函数内部的this指向的都是li元素，所有可以通过this.index获取到值
-            console.log("tab" + this.index);
-            this.className = "liactive";
-            that.sections[this.index].className = "conactive";
-        }
-        //清除所有样式
+        // 该函数的调用者为li,所以函数内部的this指向的都是li元素，clearClass函数是类的公共函数，所有只能通过that调用
+        that.clearClass();
+        // 该函数的调用者为li,所以函数内部的this指向的都是li元素，所有可以通过this.index获取到值
+        console.log("tab" + this.index);
+        this.className = "liactive";
+        that.sections[this.index].className = "conactive";
+    };
+    //清除所有样式
     clearClass() {
-            for (var i = 0; i < this.lis.length; i++) {
-                this.lis[i].className = "";
-                this.sections[i].className = "";
-            }
+        for (var i = 0; i < this.lis.length; i++) {
+            this.lis[i].className = "";
+            this.sections[i].className = "";
         }
-        // 添加功能
+    };
+    // 添加功能
     addTab() {
-            that.clearClass();
-            // 添加tab和section
-            var random = Math.random();
-            var newTab =
-                '<li class="liactive"><span>测试1</span><span class="iconfont icon-guanbi"></span></li>';
-            var newSection = `<section class="conactive">测试${random}</section>`;
-            // 在ul内部的最后一个元素的后面添加标签
-            that.ul.insertAdjacentHTML("beforeend", newTab);
-            that.fsection.insertAdjacentHTML("beforeend", newSection);
+        that.clearClass();
+        // 添加tab和section
+        var random = Math.random();
+        var newTab =
+            '<li class="liactive"><span>测试1</span><span class="iconfont icon-guanbi"></span></li>';
+        var newSection = `<section class="conactive">测试${random}</section>`;
+        // 在ul内部的最后一个元素的后面添加标签
+        that.ul.insertAdjacentHTML("beforeend", newTab);
+        that.fsection.insertAdjacentHTML("beforeend", newSection);
 
-            // 重新获取lis和sections元素，并绑定事件
-            that.init();
-        }
-        // 删除功能
+        // 重新获取lis和sections元素，并绑定事件
+        that.init();
+    };
+    // 删除功能
     deleteTab(e) {
         // ❤由于父元素绑定了切换事件，所有应该阻止冒泡
         e.stopPropagation();
@@ -97,7 +97,8 @@ class Tab {
 
         // ❤删除后重新初始化
         that.init();
-    }
+    };
+    // 编辑功能
     editTab() {
         var _that = this;
 
@@ -130,3 +131,16 @@ class Tab {
     }
 }
 var tab = new Tab("#tab");
+
+// dom相关：
+// 获取单个dom元素：document.querySelector('.fisrstnav ul:first-child')
+// 获取所有满足条件的dom元素：.querySelectorAll("li");
+// 绑定单击事件：.onclick=function(){}
+// 绑定双击事件： .ondblclick
+// 在ul的最后一个元素的后面添加标签：ul.insertAdjacentHTML("beforeend", newTab); 其他取值：beforstart,afterstart,afterend
+// 阻止冒泡：e.stopPropagation()
+// 删除元素: .remove()
+// 双击禁止选中文字：window.getSelection ?window.getSelection().removeAllRanges() :document.selection.empty();
+// 选中输入框的文字：input.select()
+// 绑定失焦事件：input.onblur=function(){}
+// 添加回车键事件：input.onkeyup=function(e){if(e.keyCode===13){.......}}
