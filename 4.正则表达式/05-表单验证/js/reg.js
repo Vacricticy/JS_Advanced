3b97fca0ef9a0e038104d1ce883aeeff803b2f47
Vacricticy <987540432@qq.com>
2020-06-10 16:45:37 +0800

    2020-06-10 finished

---
+++ 4.正则表达式/05-表单验证/js/reg.js
@@ -0,0 +1,47 @@
+window.onload = function() {
+    var regTel = /^1[3-9]\d{9}$/;
+    // console.log(regTel.test('19822906543')); //true
+    // console.log(regTel.test('1982290654')); //false
+    // console.log(regTel.test('12822906543')); //false
+    var regQq = /^[1-9]\d{4,}$/ //第一个qq号为10000
+    var regNc = /^[\u4e00-\u9fa5]{2,8}$/
+    var regMsg = /^\d{6}$/
+    var regpwd = /^[0-9a-zA-Z_-]{6,16}$/
+
+
+    var tel = document.querySelector('#tel')
+    var qq = document.querySelector('#qq')
+    var nc = document.querySelector('#nc')
+    var msg = document.querySelector('#msg')
+    var pwd = document.querySelector('#pwd')
+    var confirmpwd = document.querySelector('#surepwd')
+    reg(tel, regTel)
+    reg(qq, regQq)
+    reg(nc, regNc)
+    reg(msg, regMsg)
+    reg(pwd, regpwd)
+
+    function reg(element, reg) {
+        element.onblur = function() {
+            if (reg.test(this.value)) {
+                this.nextElementSibling.className = "success"
+                this.nextElementSibling.innerHTML = `<i class="success_icon"></i>`
+            } else {
+                this.nextElementSibling.className = "error"
+                this.nextElementSibling.innerHTML = `<i class="error_icon"></i> 格式输入有误`
+            }
+        }
+    }
+    confirmpwd.onblur = function() {
+        if (this.value === pwd.value) {
+            this.nextElementSibling.className = "success"
+            this.nextElementSibling.innerHTML = `<i class="success_icon"></i>`
+        } else {
+            this.nextElementSibling.className = "error"
+            this.nextElementSibling.innerHTML = `<i class="error_icon"></i> 两次密码输入不同`
+        }
+    }
+
+
+
+}
