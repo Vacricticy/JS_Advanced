756014c03d88821ba04a877fbcd67a69b003dea5
Vacricticy <987540432@qq.com>
2020-06-11 23:41:17 +0800

    review

--- 4.正则表达式/05-表单验证/js/reg.js
+++ 4.正则表达式/05-表单验证/js/reg.js
@@ -1,12 +1,12 @@
 window.onload = function() {
-    var regTel = /^1[3-9]\d{9}$/;
+    var regTel = /^1[3-9]\d{9}$/; //验证手机号
     // console.log(regTel.test('19822906543')); //true
     // console.log(regTel.test('1982290654')); //false
     // console.log(regTel.test('12822906543')); //false
     var regQq = /^[1-9]\d{4,}$/ //第一个qq号为10000
-    var regNc = /^[\u4e00-\u9fa5]{2,8}$/
-    var regMsg = /^\d{6}$/
-    var regpwd = /^[0-9a-zA-Z_-]{6,16}$/
+    var regNc = /^[\u4e00-\u9fa5]{2,8}$/ //昵称为2~8个汉字 
+    var regMsg = /^\d{6}$/ //验证码为6位数
+    var regpwd = /^[0-9a-zA-Z_-]{6,16}$/ //密码为6~16位的数字，英文字母，-，_的组合。
 
 
     var tel = document.querySelector('#tel')
